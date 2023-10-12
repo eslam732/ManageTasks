584da17b4cc5ff6154e70c18956ae656537b66ce
eslam732 <eaa04168@gmail.com>
2023-10-12 12:02:58 +0300

    added the bootstrab library and added bootstrab to login and sign up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Sign Up or Log In</title>
+    <title>Welcome to Task Manager</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+        /* .container {
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        } */
+    </style>
 </head>
 <body>
-    <div class="container">
-        <h1>Welcome to Task Manger</h1>
-        <p>Sign up or log in to get started.</p>
+    <div class="container d-flex flex-column justify-content-center align-items-center" style="min-height: 100vh;">
+        <h1 class="display-4 text-center mb-4">Welcome to Task Manager</h1>
+        <p class="lead text-center">Sign up or log in to get started.</p>
         <div class="button-container">
-            <a href="http://127.0.0.1:8000/signUpView" class="btn btn-primary">Sign Up</a>
-            <br/>
-            <a href="http://127.0.0.1:8000/loginView" class="btn btn-secondary">Log In</a>
+            <a href="http://127.0.0.1:8000/signUpView" class="btn btn-primary btn-lg mb-3">Sign Up</a>
+            <br>
+            <a href="http://127.0.0.1:8000/loginView" class="btn btn-secondary btn-lg">Log In</a>
         </div>
     </div>
 </body>
--- resources/views/signUp.blade.php
+++ resources/views/signUp.blade.php
@@ -2,44 +2,52 @@
 <html>
 <head>
     <title>Sign Up</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
-        <h1>Sign Up</h1>
-        <form method="POST" action="{{ route('signUp') }}">
+        <h1 class="display-4 text-center mb-4">Sign Up</h1>
+        <form method="POST" action="{{ route('signUp') }}" class="needs-validation" novalidate>
             @csrf
 
-            <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" id="name" name="name" class="form-control" value="{{ old('name') }}" required>
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" id="name" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" required>
                 @error('name')
-                 <p class="text-danger">{{ $message }}</p>
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
                 @enderror
-
             </div>
 
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" class="form-control" value="{{ old('email') }}" required>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" id="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required>
                 @error('email')
-                 <p class="text-danger">{{ $message }}</p>
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
                 @enderror
             </div>
 
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" class="form-control" required>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" id="password" name="password" class="form-control @error('password') is-invalid @enderror" required>
                 @error('password')
-                 <p class="text-danger">{{ $message }}</p>
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
                 @enderror
             </div>
 
-            <div class="form-group">
-                <label for="password_confirmation">Confirm Password</label>
-                <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" required>
+            <div class="mb-3">
+                <label for="password_confirmation" class="form-label">Confirm Password</label>
+                <input type="password" id="password_confirmation" name="password_confirmation" class="form-control @error('password_confirmation') is-invalid @enderror" required>
                 @error('password_confirmation')
-                 <p class="text-danger">{{ $message }}</p>
-                @enderror   
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
 
             <button type="submit" class="btn btn-primary">Sign Up</button>
